bb3423a44f64017049352d259e0bf8d6ba8f1b6d
Caio Vinicius <cvinicius182102@gmail.com>
2024-10-22 11:32:24 -0300

    Adicionada funcao de clear system

--- scan.sh
+++ scan.sh
@@ -1,12 +1,55 @@
 # So pra ficar bonitin
-banner(){
+bannerGeral(){
+    echo "-----------------------------------"
+    echo "        S Y S T E M A U X          "
+    echo "-----------------------------------"
+}
+bannerScanner(){
     echo "-----------------------------------"
     echo "   N E T W O R K - S C A N N E R   "
     echo "-----------------------------------"
 }
+bannerClear(){
+    echo "-----------------------------------"
+    echo "      S Y S T E M - C L E A T      "
+    echo "-----------------------------------"
+}
+# funcoes de limpeza e otimização
+limpa_cache(){
+    echo "Limpando cache do sistema..." | tee -a "$1"
+    sudo apt clean
+    sudo rm -rf "$2"/*
+    echo "Cache do sistema limpo." | tee -a "$1"
+    echo "" >> "$1"
+}
+limpa_temp() {
+    echo "Limpando arquivos temporários..." | tee -a "$1"
+    sudo rm -rf "$2"/*
+    echo "Arquivos temporários limpos." | tee -a "$1"
+    echo "" >> "$1"
+}
+limpa_logs() {
+    echo "Limpando logs antigos..." | tee -a "$1"
+    sudo find "$2" -type f -name "*.log" -exec truncate -s 0 {} \;
+    sudo rm -rf "$2"/*.gz
+    echo "Logs antigos limpos." | tee -a "$1"
+    echo "" >> "$1"
+}
+limpa_packs() {
+    echo "Removendo pacotes não utilizados..." | tee -a "$1"
+    sudo apt autoremove -y
+    sudo apt autoclean
+    echo "Pacotes obsoletos removidos." | tee -a "$1"
+    echo "" >> "$1"
+}
+update_system() {
+    echo "Atualizando o sistema..." | tee -a "$1"
+    sudo apt update && sudo apt upgrade -y
+    echo "Sistema atualizado." | tee -a "$1"
+    echo "" >> "$1"
+}
 # separa a funcao que gera o relatorio de scaneamento de rede
-# futuramente implementarei outras funcoes nesse codigo
-geraRelatorio(){
+geraRelatorioScanner(){
     echo "Relatorio de Rede - $(date)" > "$1"
     echo "-----------------------------------" > "$1"
     echo "" > "$1"
@@ -30,13 +73,42 @@ geraRelatorio(){
 
     rm tmp_ips.txt # deletando dados temporarios
 }
+geraRelatorioClear(){
+    echo "-------------------------------------" > "$1"
+    echo "Relatório de Limpeza e Otimização - $(date)" >> "$1"
+    echo "-------------------------------------" >> "$1"
+    echo "" >> "$1"
+
+    limpa_cache $1 $2
+    limpa_logs $1 $3
+    limpa_temp $1 $4
+    limpa_packs
+
+    echo "Limpeza e otimização concluídas." | tee -a "$1"
+}
 # a classica funcao main
 main(){
-    banner
-    read -p "Digite o ip da rede: " rede
-    relatorio="Relatorio_Rede$(date +%F_%T).txt"
-    echo "Lembrando que voce precisa ter o nmap em sua maquina ;)"
-    geraRelatorio $relatorio $rede
-    echo "Scanner finalizado e relatorio gerado"
+    bannerGeral
+    echo "[1] - Scanner   | [2] - System Clear   | [3] - Quit"
+    read -p "-> " decision
+
+    if [ "$decision" = "1" ]; then
+        bannerScanner
+        read -p "Digite o ip da rede: " rede
+        relatorio="Relatorio_Rede$(date +%F_%T).txt"
+        echo "Lembrando que voce precisa ter o nmap em sua maquina ;)"
+        geraRelatorioScanner $relatorio $rede
+        echo "Scanner finalizado e relatorio gerado"
+    elif [ "$decision" = "2" ]; then
+        bannerClear
+        limp_cache="var/cache"
+        limp_log="var/log"
+        limp_temp="vat/tmp"
+        limp_atp="var/lib/apt/lists"
+        relatorio="Relatorio_Limpeza_$(date +%F_%T).txt"
+        geraRelatorioClear $relatorio $limp_cache $limp_log $limp_temp $limp_atp
+         echo "Relatório salvo em: $relatorio"
+    else
+        echo "Saindo . . . "
 }
 main
